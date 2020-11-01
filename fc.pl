ef568678fce8377614ef321355d75713f3ff8562
josemorista <josemorista@id.uff.br>
2020-11-01 18:42:29 -0300

    feat: added identify questions

--- fc.pl
+++ fc.pl
@@ -5,16 +5,60 @@ has_starred(Actor, Movie) :-
 	starred_actors(Movie, Actors),
 	is_member(Actor, Actors).
 
+
+maxlist([],0, []).
+
+maxlist([Head|Tail],Max, MaxEl) :-
+    maxlist(Tail,TailMax, MaxEl),
+		string_length(Head, Len),
+    Len > TailMax,
+		MaxEl is Head,
+    Max is Head.
+
+maxlist([Head|Tail],Max, MaxEl) :-
+    maxlist(Tail,TailMax, MaxEl),
+    string_length(Head, Len),
+		Len =< TailMax,
+		MaxEl is TailMax,
+    Max is TailMax.
+
 are_about(Search, Movie) :- 
 	has_tags(Movie, Tags),
 	is_member(Search, Tags).
-	
-be_described_by(X, Movie) :-
-	are_about(X, Movie);
-	has_starred(X, Movie);
-	directed_by(X, Movie).
 
-identify_question(Question) :-
-	split_string(Question, "\s", "\s", L),
-	is_member("directed", L),
-	write('A question about directors, nice!').
+includes(Str, SubString) :-
+	sub_string(Str, _, _, _, SubString).
+
+split_string_with_substring(String, Before, After, SubString) :-
+  sub_string(String, Bef, _, Aft, SubString), !,
+  sub_string(String, 0, Bef, _, NameString),
+  atom_string(Before, NameString),
+  sub_string(String, _, Aft, 0, After).
+
+identify_question(Question, X) :-
+	(includes(Question, "director");
+	includes(Question, "directs")),
+	includes(Question, Movie),
+	directed_by(Movie, X).
+	
+identify_question(Question, X) :-
+	(split_string_with_substring(Question, _, After, "are about");
+	split_string_with_substring(Question, _, After, "be described")),
+	findall(Y, (includes(After, Y),are_about(Y, _)), L),
+	find_biggest(L, Y, 0),
+	findall(Z, are_about(Y, Z), X).
+
+identify_question(Question, X) :-
+	(includes(Question, "acts");
+	includes(Question, "did act");
+	includes(Question, "starred")),
+	findall(Y, (includes(Question, Y),has_starred(Y, _)), L),
+	find_biggest(L, Y, 0),
+	findall(Z, has_starred(Y, Z), X).
+
+ask :-
+	write("Hello human, what do you want to know about movies?"),nl,
+	read(Question),
+	includes(Question, "?"),
+	identify_question(Question, X),
+	write(X).
--- index.ts
+++ index.ts
@@ -12,8 +12,6 @@ const facts: { [key: string]: Array<string> } = {
 	'in_language': [],
 };
 
-const movies: Array<string> = [];
-
 const data = fs.readFileSync(path.resolve(__dirname, 'moviesData.txt'), { encoding: 'utf-8' });
 
 data.split(/\r?\n/).forEach(line => {
@@ -21,10 +19,6 @@ data.split(/\r?\n/).forEach(line => {
 	if (describes) {
 		const [indexAndMovie, originalValue] = line.replace(new RegExp('\'', 'g'), '').replace(new RegExp('"', 'g'), '').split(describes);
 		const movie = indexAndMovie.slice(2).trim();
-
-		if (!movies.includes(movie)) {
-			movies.push(movie);
-		}
 
 		let value = originalValue.trim();
 
@@ -44,5 +38,4 @@ for (const key in facts) {
 	dataToWrite += `\n${facts[key].join('\n')}`;
 }
 
-fs.writeFileSync(path.resolve(__dirname, 'mv.pl'), movies.map(el => `movie("${el}").`).join('\n'));
 fs.writeFileSync(path.resolve(__dirname, 'db.pl'), dataToWrite);
