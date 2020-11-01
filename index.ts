c1e8f6d81c2aff1a13e15aee3051e7f42b80fc68
josemorista <josemorista@id.uff.br>
2020-11-01 11:48:21 -0300

    feat: added movies list

--- index.ts
+++ index.ts
@@ -9,8 +9,10 @@ const facts: { [key: string]: Array<string> } = {
 	'release_year': [],
 	'has_genre': [],
 	'has_tags': [],
-	'in_language': []
+	'in_language': [],
 };
+
+const movies: Array<string> = [];
 
 const data = fs.readFileSync(path.resolve(__dirname, 'moviesData.txt'), { encoding: 'utf-8' });
 
@@ -18,7 +20,12 @@ data.split(/\r?\n/).forEach(line => {
 	const describes = Object.keys(facts).find(el => line.includes(el));
 	if (describes) {
 		const [indexAndMovie, originalValue] = line.replace(new RegExp('\'', 'g'), '').replace(new RegExp('"', 'g'), '').split(describes);
-		const movie = indexAndMovie.replace(/[0-9]/g, '').trim();
+		const movie = indexAndMovie.slice(2).trim();
+
+		if (!movies.includes(movie)) {
+			movies.push(movie);
+		}
+
 		let value = originalValue.trim();
 
 		if (describes === 'starred_actors' || describes === 'has_tags' || describes === 'has_genre') {
@@ -37,4 +44,5 @@ for (const key in facts) {
 	dataToWrite += `\n${facts[key].join('\n')}`;
 }
 
+fs.writeFileSync(path.resolve(__dirname, 'mv.pl'), movies.map(el => `movie('${el}').`).join('\n'));
 fs.writeFileSync(path.resolve(__dirname, 'db.pl'), dataToWrite);
