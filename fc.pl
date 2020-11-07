79f953b430cb2dc2e9562db4b23a70802014b702
josemorista <josemorista@id.uff.br>
2020-11-07 14:09:36 -0300

    feat: better user interactor

--- fc.pl
+++ fc.pl
@@ -14,7 +14,7 @@ has_common_member([H| Tail], L) :-
 	is_member(H, L);
 	has_common_member(Tail, L).
 
-suggest_me([], X) :- true.
+suggest_me([], _) :- true.
 
 suggest_me([Movie| Tail], X) :- 
 	((has_genre(Movie, G),
@@ -30,41 +30,49 @@ includes(Str, SubString) :-
 	sub_string(Str, _, _, _, SubString).
 
 identify_question(Question) :-
-	(includes(Question, "suggest me");
-	includes(Question, "recomend me")),
+	(includes(Question, "suggest");
+	includes(Question, "recomend")),
 	write("Tell me a list of movies you like and i will find the right movie for you!"),nl,
 	read(Y),
 	suggest_me(Y, L),
-	write(L).
+	write("My recomendation is: "),
+	write(L),
+	write(", Hope you like it ;)").
 
 identify_question(Question) :-
 	(includes(Question, "are about");
 	includes(Question, "be described")),
 	write("What movie or subject are you talking about?"),nl,
 	read(Y),
-	((are_about(Y, _),findall(Z, are_about(Y,Z), L));
-	(are_about(_, Y),findall(Z, are_about(Z,Y), L))),
+	((are_about(Y, _),findall(Z, are_about(Y,Z), L), write("This movie is about: "));
+	(are_about(_, Y),findall(Z, are_about(Z,Y), L), write("I found a nice list of movies for you: "))),
+	nl, 
 	write(L).
 
 identify_question(Question) :-
 	(includes(Question, "director");
+	includes(Question, "directed");
 	includes(Question, "directs")),
 	write("A directors question!!! Tell me the movie you want to know who is the director or the name of one director and i will tell which movies has his/her touch ;)"),nl,
 	read(Y),
-	((directed_by(Y, _),findall(Z, directed_by(Y,Z), L));
-	(directed_by(_, Y),findall(Z, directed_by(Z,Y), L))),
+	((directed_by(Y, _),findall(Z, directed_by(Y,Z), L), write("Directed by: "));
+	(directed_by(_, Y),findall(Z, directed_by(Z,Y), L),write("Found this movies from this director, gold list here: "))),
+	nl,
 	write(L).
 
 identify_question(Question) :-
 	(includes(Question, "acts");
 	includes(Question, "did act");
+	includes(Question, "actor");
+	includes(Question, "actress");
 	includes(Question, "starred")),
 	write("An actors question!!! I love Meryl Streep, and you? Tell me a movie or actor/actress :)"),nl,
 	read(Y),
-	((has_starred(_, Y),findall(Z, has_starred(Z,Y), L));
-	starred_actors(Y, L)),
+	((has_starred(_, Y),findall(Z, has_starred(Z,Y), L), write("These are the movies that i found: "));
+	(starred_actors(Y, L),write("These are the stars of this movie: "))),
+	nl, 
 	write(L).
- 
+	
 
 hi :-
 	write("Hello human, what do you want to know about movies?"),nl,
